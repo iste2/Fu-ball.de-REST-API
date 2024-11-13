1c46fe7cb07f49b1627cdf29e894bc3d16b5f777
lnadenau <lukas.nadenau@inform-software.com>
2024-11-13 15:00:18 +0100

    Kommentare entfernt

--- Fußball.de.Rest.Api/Program.cs
+++ Fußball.de.Rest.Api/Program.cs
@@ -4,15 +4,12 @@ using Fußball.de.Scraping;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddOutputCache();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
 
